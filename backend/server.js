ca87b8c133727210bb3f41ec2109f1cf62581700
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-15 14:37:27 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = 3000
 const host = 'localhost'
 
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://${host}:${PORT}`)
--- backend/test/route.js
+++ backend/test/route.js
@@ -4,6 +4,9 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   console.log('router')
   res.send('Hi there')
+}).post((req, res) => {
+  console.log('post')
 });
+
 
 module.exports = router;
