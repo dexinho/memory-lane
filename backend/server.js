3237918e6942ea80bf78af5a2c2d1c3c3b5ed84b
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-17 23:41:21 +0100

    added router

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const validateLoginRouter = require('./routers/validateLoginRouter')
+const getTimelinesRouter = require('./routers/getTimelinesRouter')
 
-const PORT = 3000
-const host = 'localhost'
+const PORT = 3000;
+const host = "localhost";
 
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world')
-})
+app.use("/validateLogin", validateLoginRouter);
+app.use('/getTimelines', getTimelinesRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://${host}:${PORT}`)
-})
-
+  console.log(`Server listening on http://${host}:${PORT}`);
+});
