80120fc454cac8591190ce237ad4a1a479efa927
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-17 23:42:17 +0100

    added form event listener

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,40 @@
-const timelinePics = document.querySelectorAll('.timeline-picture-t')
+const loginForm = document.querySelector("#login-form");
+const submitBtn = document.querySelector("#submit-btn");
+import { displayLoggedUser, displayTimelines } from "./src/utility/js/loginSuccessful.js";
+import { host, PORT } from "./src/utility/js/url.js";
 
-timelinePics.forEach(timelinePic => {
-  timelinePic.addEventListener('click', () => {
-    const cover = document.createElement('div')
-    document.body.append(cover)
+submitBtn.addEventListener("click", async (e) => {
+  const formEntries = new FormData(loginForm);
+  const url = `http://${host}:${PORT}`
+  e.preventDefault();
 
-    cover.classList.toggle('body-cover')
-    document.body.classList.toggle('overflow')
-  })
-})
+  try {
+    const response = await fetch(`${url}/validateLogin`, {
+      method: "POST",
+      body: JSON.stringify(Object.fromEntries(formEntries)),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
+    if (response.ok) {
+      const loggedUserData = await response.json();
+      const getTimelines = await fetch(`${url}/getTimelines`)
+      const timelines = await getTimelines.json()
+
+      console.log(loggedUserData)
+      console.log(timelines)
+
+      // displayLoggedUser(loggedUserData)
+      // displayTimelines(timelines)
+    } else if (response.status === 404){
+      console.log('login failed')
+      // loginFailed()
+    } else {
+      console.log('server error')
+    }
+
+  } catch (err) {
+    console.log(err);
+  }
+});
