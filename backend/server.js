7ada106a3148271e1febfce6d1f673d8cdbc13ec
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-18 05:29:27 +0100

    added image blob representation

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const validateLoginRouter = require('./routers/validateLoginRouter')
-const getTimelinesRouter = require('./routers/getTimelinesRouter')
+const loginRouter = require("./routers/loginRouter");
+const getTimelinesRouter = require("./routers/getTimelinesRouter");
+const uploadPictureRouter = require("./routers/uploadPictureRouter");
 
 const PORT = 3000;
 const host = "localhost";
 
+app.use(express.raw({ type: "application/octet-stream", limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.use("/validateLogin", validateLoginRouter);
-app.use('/getTimelines', getTimelinesRouter)
+app.use("/login", loginRouter);
+app.use("/getTimelines", getTimelinesRouter);
+app.use("/uploadPicture", uploadPictureRouter);
+
+app.post("/test", (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://${host}:${PORT}`);
