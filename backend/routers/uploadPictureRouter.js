45d75aea78615753db54429fcb55ac62a255a564
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-20 01:36:27 +0100

    changed multer to blob

--- backend/routers/uploadPictureRouter.js
+++ backend/routers/uploadPictureRouter.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const uploadPicture = require("../middleware/uploadPicture");
 const uploadPictureController = require("../controller/uploadPictureController");
 
-router.post("/", uploadPicture.single("image"), uploadPictureController);
+router.post("/", uploadPictureController);
 
 module.exports = router;
