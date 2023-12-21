80897067dc8d3fa2760c11d755cc61bedf72dc23
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-21 19:51:22 +0100

    server now handles pictures

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const loginRouter = require("./routers/loginRouter");
-const getTimelinesRouter = require("./routers/getTimelinesRouter");
-const uploadPictureRouter = require("./routers/uploadPictureRouter");
+const timelinesRouter = require("./routers/timelinesRouter");
+const picturesRouter = require("./routers/picturesRouter");
 
 const PORT = 3000;
 const host = "localhost";
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.raw({ type: "application/octet-stream", limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 app.use("/login", loginRouter);
-app.use("/getTimelines", getTimelinesRouter);
-app.use("/uploadPicture", uploadPictureRouter);
+app.use("/timelines", timelinesRouter);
+app.use("/pictures", picturesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://${host}:${PORT}`);
