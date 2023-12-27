f373194f24b1a7381252a7e53b7872864c95c23a
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-28 00:00:01 +0100

    changed to type module

--- frontend/src/utility/js/querySelectors.js
+++ frontend/src/utility/js/querySelectors.js
@@ -1,17 +1,17 @@
 // Divs
-const userNameT = document.querySelector(".user-name-t");
-const timelinesDivT = document.querySelector("#timelines-div-t");
-const socialMediaLogin = document.querySelector("#social-media-login");
-const loginPopUpMsg = document.querySelector("#login-pop-up-msg");
-const timelineUserProfileT = document.querySelector("#timeline-user-profile-t");
-const timelinePicDivT = document.querySelectorAll(".timeline-picture-div-t");
-const userProfilePicT = document.querySelector("#user-profile-pic-div-t");
+export const userNameT = document.querySelector(".user-name-t");
+export const timelinesDivT = document.querySelector("#timelines-div-t");
+export const socialMediaLogin = document.querySelector("#social-media-login");
+export const loginPopUpMsg = document.querySelector("#login-pop-up-msg");
+export const timelineUserProfileT = document.querySelector("#timeline-user-profile-t");
+export const timelinePicDivT = document.querySelectorAll(".timeline-picture-div-t");
+export const userProfilePicT = document.querySelector("#user-profile-pic-div-t");
 
 // Buttons
-const submitBtn = document.querySelector("#submit-btn");
-const logOutBtnT = document.querySelector("#log-out-btn-t");
+export const submitBtn = document.querySelector("#submit-btn");
+export const logOutBtnT = document.querySelector("#log-out-btn-t");
 
 // Images
 
 // Forms
-const loginForm = document.querySelector("#login-form");
+export const loginForm = document.querySelector("#login-form");
