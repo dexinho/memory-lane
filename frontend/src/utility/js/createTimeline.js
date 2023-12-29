d28af6576e77b1689ee50044c833bffe0f21d796
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-29 06:15:42 +0100

    temporary timeline creation

--- frontend/src/utility/js/createTimeline.js
+++ frontend/src/utility/js/createTimeline.js
@@ -1,13 +1,14 @@
+import { URL } from "./globalVar.js";
+
 const pictureInput = document.querySelector("#picture-input");
 
 pictureInput.addEventListener("change", async () => {
   const file = pictureInput.files[0];
-  const url = `http://${host}:${PORT}`;
 
   if (file) {
     const blob = new Blob([file]);
 
-    await fetch(`${url}/upload/picture`, {
+    await fetch(`${URL}/upload/picture`, {
       method: "POST",
       body: blob,
       headers: {
