78ba775b35a20ed9b1acc163e59d8c75d56e1935
Jasmin Hidanovic <dexinho008@gmail.com>
2023-12-29 06:16:11 +0100

    auth for login and register

---
+++ backend/models/authenticationModel.js
@@ -0,0 +1,36 @@
+const connection = require("./connectionPool");
+
+const authenticationModel = {};
+
+const validate = async ({ email_input, password_input }) => {
+  const [data] = await connection.execute(
+    `SELECT user_id FROM users where user_email = ? AND user_password_hash = ?`,
+    [email_input, password_input]
+  );
+
+  if (data.length > 0) return data[0];
+};
+
+const register = async ({
+  email_input_r,
+  password_input_r,
+  first_name_input_r,
+  last_name_input_r,
+}) => {
+  try {
+    await connection.execute(
+      `INSERT INTO users (user_email, user_password_hash, user_first_name, user_last_name) VALUES (?, ?, ?, ?)`,
+      [email_input_r, password_input_r, first_name_input_r, last_name_input_r]
+    );
+
+    return '00000'
+  } catch (err) {
+    if (err.sqlState === "23000") return "23000";
+    else return;
+  }
+};
+
+authenticationModel.validate = validate;
+authenticationModel.register = register;
+
+module.exports = authenticationModel;
