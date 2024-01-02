3026615f72c830eea751e2efa6cd1625bf394126
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-02 23:46:05 +0100

    added password hash

--- backend/models/authenticationModel.js
+++ backend/models/authenticationModel.js
@@ -1,11 +1,14 @@
 const connection = require("./connectionPool");
+const customHash = require("../utils/customHash");
 
 const authenticationModel = {};
 
 const validate = async ({ email_input, password_input }) => {
+  const hash = customHash(password_input, email_input);
+
   const [data] = await connection.execute(
     `SELECT user_id FROM users where user_email = ? AND user_password_hash = ?`,
-    [email_input, password_input]
+    [email_input, hash]
   );
 
   if (data.length > 0) return data[0];
@@ -18,12 +21,14 @@ const register = async ({
   last_name_input_r,
 }) => {
   try {
+    const hash = customHash(password_input_r, email_input_r);
+
     await connection.execute(
       `INSERT INTO users (user_email, user_password_hash, user_first_name, user_last_name) VALUES (?, ?, ?, ?)`,
-      [email_input_r, password_input_r, first_name_input_r, last_name_input_r]
+      [email_input_r, hash, first_name_input_r, last_name_input_r]
     );
 
-    return '00000'
+    return "00000";
   } catch (err) {
     if (err.sqlState === "23000") return "23000";
     else return;
