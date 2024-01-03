d46f8ff047beda1073815b2b90dbbe6237d972a5
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-03 23:58:48 +0100

    added user search

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -4,5 +4,6 @@ const usersController = require("../controllers/usersController");
 
 router.get("/profile-pictures", usersController.getProfilePicture);
 router.get("/data", usersController.data);
+router.get('/search', usersController.search)
 
 module.exports = router;
