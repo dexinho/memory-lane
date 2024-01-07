cb809a88d419aa413445a9619559f4b51fab6c8a
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-07 11:24:30 +0100

    added more selectors

--- frontend/src/utility/js/querySelectors.js
+++ frontend/src/utility/js/querySelectors.js
@@ -2,7 +2,7 @@
 export const userNameT = document.querySelector(".user-name-t");
 export const timelinesDivT = document.querySelector("#timelines-div-t");
 export const socialMediaLogin = document.querySelector("#social-media-login");
-export const loginPopUpMsg = document.querySelector("#login-pop-up-msg");
+export const loginPopUpMsgT = document.querySelector("#login-pop-up-msg-t");
 export const timelineUserProfileT = document.querySelector("#timeline-user-profile-t");
 export const timelinePicDivT = document.querySelectorAll(".timeline-picture-div-t");
 export const userProfilePicT = document.querySelector("#user-profile-pic-div-t");
