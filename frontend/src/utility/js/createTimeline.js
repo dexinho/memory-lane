96e849d427cf5b30c57c98f4c08eb8a71b77793d
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-07 11:07:25 +0100

    added modal show/close

--- frontend/src/utility/js/createTimeline.js
+++ frontend/src/utility/js/createTimeline.js
@@ -1,19 +1,38 @@
 import { URL } from "./globalVar.js";
 
 const pictureInput = document.querySelector("#picture-input");
+const timelineCreationDialogT = document.querySelector(
+  "#timeline-creation-dialog-t"
+);
+const createNewTimelineBtnT = document.querySelector(
+  "#create-new-timeline-btn-t"
+);
+const closeTimelineCreationBtnT = document.querySelector(
+  "#close-timeline-creation-btn-t"
+);
 
-pictureInput.addEventListener("change", async () => {
-  const file = pictureInput.files[0];
+// pictureInput.addEventListener("change", async () => {
+//   const file = pictureInput.files[0];
 
-  if (file) {
-    const blob = new Blob([file]);
+//   if (file) {
+//     const blob = new Blob([file]);
 
-    await fetch(`${URL}/upload/picture`, {
-      method: "POST",
-      body: blob,
-      headers: {
-        "Content-Type": "application/octet-stream",
-      },
-    });
-  }
-});
+//     await fetch(`${URL}/upload/picture`, {
+//       method: "POST",
+//       body: blob,
+//       headers: {
+//         "Content-Type": "application/octet-stream",
+//       },
+//     });
+//   }
+// });
+
+export const handleTimelineCreation = () => {
+  createNewTimelineBtnT.addEventListener("click", () => {
+    timelineCreationDialogT.showModal();
+  });
+
+  closeTimelineCreationBtnT.addEventListener("click", () => {
+    timelineCreationDialogT.close();
+  });
+};
