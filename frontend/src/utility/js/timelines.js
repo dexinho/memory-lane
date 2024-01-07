840c9bddf8689c4aca82cf582b2f139d53268403
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-07 11:25:03 +0100

    added dynamic scrolls that scrolls slot by slot

--- frontend/src/utility/js/timelines.js
+++ frontend/src/utility/js/timelines.js
@@ -7,3 +7,44 @@
 //     document.body.classList.toggle("overflow");
 //   });
 // });
+
+const timelineMemorySlotsT = document.querySelector(".timeline-memory-slots-t");
+const timelineMemorySlotT = document.querySelectorAll(
+  ".timeline-memory-slot-t"
+);
+
+export const scroll = () => {
+  let prevScrollPosition = timelineMemorySlotsT.scrollTop;
+
+  timelineMemorySlotsT.addEventListener("scroll", () => {
+    let slotHeight = parseInt(
+      getComputedStyle(timelineMemorySlotT[0]).height,
+      10
+    );
+    let slotPadding = parseInt(
+      getComputedStyle(timelineMemorySlotsT).paddingTop,
+      10
+    );
+
+    let scrollOffset = slotHeight + slotPadding;
+    let currentScrollPosition = timelineMemorySlotsT.scrollTop;
+
+    if (currentScrollPosition > prevScrollPosition) {
+      const nextSnapPosition =
+        Math.ceil(currentScrollPosition / scrollOffset) * scrollOffset;
+      timelineMemorySlotsT.scrollTo({
+        top: nextSnapPosition,
+        behavior: "smooth",
+      });
+    } else {
+      const prevSnapPosition =
+        Math.floor(currentScrollPosition / scrollOffset) * scrollOffset;
+      timelineMemorySlotsT.scrollTo({
+        top: prevSnapPosition,
+        behavior: "smooth",
+      });
+    }
+
+    prevScrollPosition = currentScrollPosition;
+  });
+};
