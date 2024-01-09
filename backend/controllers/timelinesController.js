c6d717b84cab1d404f108eedb9cce1d2f59bc089
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-09 13:56:58 +0100

    added postTimeline and addMemory functions

--- backend/controllers/timelinesController.js
+++ backend/controllers/timelinesController.js
@@ -7,8 +7,6 @@ const getTimelines = async (req, res) => {
     const amount = req.query?.amount;
     const timelines = await timelinesModel.getTimelines(amount);
 
-    // console.log("Timelines", timelines);
-
     res.status(200).json(timelines);
   } catch (err) {
     console.log(err);
@@ -16,6 +14,33 @@ const getTimelines = async (req, res) => {
   }
 };
 
+const postTimeline = async (req, res) => {
+  console.log(req.body);
+  try {
+    const timelineID = await timelinesModel.postTimeline(req.body);
+
+    if (timelineID) res.status(200).json(timelineID);
+    else res.status(500);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+};
+
+const addMemory = async (req, res) => {
+  try {
+    const sqlState = await timelinesModel.addMemory(req.body);
+
+    if (sqlState === "00000") res.sendStatus(200);
+    else if (sqlState === "23000") res.sendStatus(409);
+    else res.sendStatus(500);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+};
+
 timelinesController.getTimelines = getTimelines;
+timelinesController.postTimeline = postTimeline;
+timelinesController.addMemory = addMemory;
 
 module.exports = timelinesController;
