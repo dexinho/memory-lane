f6f9915aec943da1064bb94d86418c5e7081ee8a
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-10 08:33:47 +0100

    added post timeline and add memory routes

--- backend/routers/timelinesRouter.js
+++ backend/routers/timelinesRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const timelinesController = require("../controllers/timelinesController.js");
 
 router.get("/get-timelines", timelinesController.getTimelines);
+router.post('/post-timeline', timelinesController.postTimeline)
+router.post('/add-memory', timelinesController.addMemory)
 
 module.exports = router;
