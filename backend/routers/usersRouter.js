02e0dfc5946cd651cfb6886480e7f2639094e1a8
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-11 16:55:34 +0100

    changed data to getUserData

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 
-router.get("/profile-pictures", usersController.getProfilePicture);
-router.get("/data", usersController.data);
+router.get("/get-profile-picture", usersController.getProfilePicture);
+router.get("/get-user-data", usersController.getUserData);
 router.get('/search', usersController.search)
 
 module.exports = router;
