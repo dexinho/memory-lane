9aea94c78c438d9935bfbf9fcc83a808cee0e376
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-11 16:55:16 +0100

    changed data to getUserData

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -2,46 +2,28 @@ const connection = require("./connectionPool");
 
 const usersModel = {};
 
-const getProfilePicture = async () => {
-  const [data] = await connection.execute(`SELECT
-    u.user_id,
-    u.user_email,
-    u.user_password_hash,
-    u.user_first_name,
-    u.user_last_name,
+const getProfilePicture = async (userID) => {
+  const [data] = await connection.execute(
+    `SELECT
     up.picture_data AS user_picture_data
     FROM
     users u
     LEFT JOIN
-    pictures up ON u.user_picture_id = up.picture_id`);
+    pictures up ON u.user_picture_id = up.picture_id
+    WHERE user_id = ?`,
+    [userID]
+  );
 
   return data;
 };
 
-const data = async (id) => {
+const getUserData = async (id) => {
   try {
-    // let query = `SELECT user_id, picture_data, user_first_name, user_last_name from users join pictures on users.user_picture_id = pictures.picture_id`;
-
-    // const params = [];
-
-    // console.log(id)
-
-    // if (id) {
-    //   query += " WHERE user_id = ?";
-    //   params.push(id);
-    // }
-
-    // const [userData] = await connection.execute(query, params);
-
-    console.log("id", id);
-
     const [userData] = await connection.execute(
       `SELECT user_id, picture_data, user_first_name, user_last_name from users join pictures on users.user_picture_id = pictures.picture_id WHERE user_id = ?`,
       [id]
     );
-
-    console.log(userData);
-
+    
     return userData[0];
   } catch (err) {
     console.log(err);
@@ -65,7 +47,7 @@ const search = async (query) => {
 };
 
 usersModel.getProfilePicture = getProfilePicture;
-usersModel.data = data;
+usersModel.getUserData = getUserData;
 usersModel.search = search;
 
 module.exports = usersModel;
