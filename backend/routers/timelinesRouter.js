771dc83283a3d86a012d66c51bb04ff00be21a84
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-15 14:30:22 +0100

    added timeline deletion

--- backend/routers/timelinesRouter.js
+++ backend/routers/timelinesRouter.js
@@ -4,10 +4,13 @@ const timelinesController = require("../controllers/timelinesController.js");
 
 router.get("/get-timelines", timelinesController.getTimelines);
 router.get('/get-memories', timelinesController.getMemories)
+router.get('/get-timeline-view-count', timelinesController.getTimelineViewCount)
 
 router.post('/post-timeline', timelinesController.postTimeline)
 router.post('/post-memory', timelinesController.postMemory)
 router.post('/post-timeline-visit', timelinesController.postTimelineVisit)
+
+router.delete('/delete-timeline/:timeline_id', timelinesController.deleteTimeline)
 
 
 module.exports = router;
